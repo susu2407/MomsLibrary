5e6e768ca73163b6efb96c9ca53e2e295440181b
tndus159200297 <tndus159200297@gmail.com>
2026-04-28 20:18:56 +0900

    feat: service 작성

--- src/main/java/com/susuproject/MomsLibrary/service/DocumentService.java
+++ src/main/java/com/susuproject/MomsLibrary/service/DocumentService.java
@@ -1,17 +1,67 @@
 package com.susuproject.MomsLibrary.service;
 
+import com.susuproject.MomsLibrary.model.CategoryEntity;
+import com.susuproject.MomsLibrary.model.DocumentEntity;
+import com.susuproject.MomsLibrary.repository.DocumentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DocumentService {
+
+    // DB 접근 객체(변경 불가(final)) + 생성자 주입
+    private final DocumentRepository documentRepository;
+
+    public DocumentService(DocumentRepository documentRepository) {
+        this.documentRepository = documentRepository;
+    }
+
+
     //자료 전체 목록 조회 (최신순)
-    //자료 단건 조회 (id로)
+    public List<DocumentEntity> getAllDocuments() {
+        return documentRepository.findAll();
+    }
+
     //자료 등록 (code 자동 생성 포함)
+    public DocumentEntity createDocument(DocumentEntity documentEntity) {
+        return documentRepository.save(documentEntity);
+    }
+
     //자료 수정
+    public DocumentEntity updateDocument(DocumentEntity documentEntity) {
+        return documentRepository.save(documentEntity);
+    }
+
     //자료 삭제
+    public void deleteDocument(int id) {
+        documentRepository.deleteById(id);
+    }
+
     //제목으로 검색
+    public List<DocumentEntity> searchTitleDocument(String title) {
+        return documentRepository.findByTitleContaining(title);
+    }
+
     //저자로 검색
+    public List<DocumentEntity> searchAuthorDocument(String author) {
+        return documentRepository.findByAuthorContaining(author);
+    }
+
     //제목+저자 통합 검색
+    public List<DocumentEntity> searchTitleOrAuthorDocument(String title, String author) {
+        return documentRepository.findByTitleContainingOrAuthorContaining(title, author);
+    }
+
     //카테고리로 필터링
+    public List<DocumentEntity> filteringCategoryDocument(CategoryEntity category) {
+        return documentRepository.findByCategory(category);
+    }
+
+    // ------ 정렬 -------------------------
+    // 최신순 정렬
+    // 제목 가나다 정렬
+    // 제목 하파타 정렬
+    // No 정렬
 
 }
--- src/main/java/com/susuproject/MomsLibrary/service/TagService.java
+++ src/main/java/com/susuproject/MomsLibrary/service/TagService.java
@@ -1,10 +1,40 @@
 package com.susuproject.MomsLibrary.service;
 
+import com.susuproject.MomsLibrary.model.TagEntity;
+import com.susuproject.MomsLibrary.repository.TagRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TagService {
+
+    // DB 연결 + 생성자
+    private final TagRepository tagRepository;
+
+    public TagService(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
     //전체 태그 목록 조회
+    public List<TagEntity> findAll() {
+        return tagRepository.findAll();
+    }
+
     //태그 등록 (중복 체크 포함)
+    public TagEntity createdTag(String name) {
+//        TagEntity tag = new TagEntity(name);
+//        return tagRepository.save(tag);       간단하게 작성 가능.
+
+        return tagRepository.save(new TagEntity(name));
+    }
+    //태그 수정
+    public TagEntity updatedTag(String name) {
+        return tagRepository.save(new TagEntity(name));
+    }
+
     //태그 삭제
+    public void deletedTag(String name) {
+        tagRepository.delete(new TagEntity(name));
+    }
 }
