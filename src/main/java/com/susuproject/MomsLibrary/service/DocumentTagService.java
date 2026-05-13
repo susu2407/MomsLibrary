8e6874ee7da85824c1f784965ecda29763d575c9
tndus159200297 <tndus159200297@gmail.com>
2026-05-13 12:07:00 +0900

    feat:DocumentTagService.java 추가

---
+++ src/main/java/com/susuproject/MomsLibrary/service/DocumentTagService.java
@@ -0,0 +1,10 @@
+package com.susuproject.MomsLibrary.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DocumentTagService {
+    //    자료에 태그 연결
+    //    자료에서 태그 해제
+    //    특정 자료의 태그 목록 조회
+}
